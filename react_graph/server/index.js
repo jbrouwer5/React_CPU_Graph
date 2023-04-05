bb4aaaf469e71798bca17301dbf10f1db6710d04
Jackson Brouwer <jbrouwer@uchicago.edu>
2023-04-05 22:54:29 +0200

    Rearranged files

--- react_graph/server/index.js
+++ react_graph/server/index.js
@@ -5,14 +5,14 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-app.use(express.static(path.resolve(__dirname, "../react_graph/build")));
+app.use(express.static(path.resolve(__dirname, "../build")));
 
 app.get("/api", (req, res) => {
   res.json({ message: "Hello World!" });
 });
 
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(_dirname, "../react_graph/build", "index.html"));
+  res.sendFile(path.resolve(_dirname, "../build", "index.html"));
 });
 
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
